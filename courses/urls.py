6753bc77683d6b2e787d3efd39be1f45b9771025
Code021-raelle <gabrielakinshola3@gmail.com>
2025-01-23 18:52:27 +0100

    urls

--- courses/urls.py
+++ courses/urls.py
@@ -3,11 +3,12 @@ from . import views
 from .views import CourseListView
 
 urlpatterns = [
-    path('', views.course_list, name='course_list'),
+    path('', views.landing_page, name='landing_page'),
+    path('courses/list', views.course_list, name='course_list'),
     path('courses/create/', views.course_create, name='course_create'),
     path('<int:pk>/', views.course_detail, name='course_detail'),
     path('instructor/dashboard/', views.instructor_dashboard, name='instructor_dashboard'),
-    path('<int:course_id>/add_lesson/', views.add_lesson, name='add_lesson'),
+    path('course/<int:course_id>/add_lesson/', views.add_lesson, name='add_lesson'),
     path('lessons/<int:lesson_id>/complete/', views.complete_lesson, name='complete_lesson'),
     path('course/<int:course_id>/certificate/', views.generate_certificate, name='generate_certificate'),
     path('course/<int:course_id>/review/', views.add_review, name='add_review'),
@@ -18,9 +19,14 @@ urlpatterns = [
     path('<int:course_id>/payment/', views.course_payment, name='course_payment'),
     path('course/<int:course_id>/threads/', views.course_threads, name='course_threads'),
     path('course/<int:course_id>/threads/create/', views.create_thread, name='create_thread'),
+    path('course/<int:course_id>/threads/<int:thread_id>/', views.thread_replies, name='thread_replies'),
     path('dashboard/analytics/', views.analytics_dashboard, name='analytics_dashboard'),
     path('gamification/badges/', views.user_badges, name='user_badges'),
     path('gamification/leaderboard/', views.view_leaderboard, name='view_leaderboard'),
-    path('profiles/<str:username>/', views.user_profile, name="user_profile"),
+    path('profiles/<str:username>/', views.user_profile, name='user_profile'),
+    path('profile/<str:user>/edit/', views.edit_profile, name='edit_profile'),
+    path('inbox/', views.inbox, name='inbox'),
+    path('chats/', views.send_message, name='send_message'),
+    path('chat/', views.chat, name='chat'), 
     path('api/courses/', CourseListView.as_view(), name='api_courses'),
 ]
