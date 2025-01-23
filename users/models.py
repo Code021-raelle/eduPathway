ad36748da22e88d544a0571aeea40e0f60078db6
Code021-raelle <gabrielakinshola3@gmail.com>
2025-01-23 18:31:25 +0100

    user models

--- users/models.py
+++ users/models.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 class CustomUser(AbstractUser):
     """
     Custom user model extending Django's AbstractUser.
     Includes additional fields to distinguish user roles.
     """
+    
+
     is_instructor = models.BooleanField(default=False, verbose_name="Is Instructor")
     is_student = models.BooleanField(default=True, verbose_name="Is Student")
     
@@ -16,3 +19,4 @@ class CustomUser(AbstractUser):
         if self.is_instructor and self.is_student:
             raise ValueError("A user cannot be both an instructor and a student.")
         super().save(*args, **kwargs)
+
