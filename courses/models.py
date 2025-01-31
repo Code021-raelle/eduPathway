6421a79c921fa2098043540580b37b86f6b0bce5
Gabriel Akinshola <gabrielakinshola3@gmail.com>
2025-01-31 18:14:39 +0100

    removed userprofile to users.model

--- courses/models.py
+++ courses/models.py
@@ -160,28 +160,6 @@ class UserProgress(models.Model):
     quiz_scores = models.JSONField(default=dict)
 
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='userprofile')
-    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
-    bio = models.TextField(blank=True, null=True)
-    interests = models.ManyToManyField('Tag', blank=True)
-    completed_courses = models.ManyToManyField(Course, blank=True)
-    last_seen = models.DateTimeField(auto_now=True)     # For "last seen" status
-    status_message = models.TextField(blank=True, null=True)
-    status_image = models.ImageField(upload_to='status/', blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.user.username}'s Profile"
-
-@receiver(post_save, sender=CustomUser)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=CustomUser)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
 class StudyGroup(models.Model):
     name = models.CharField(max_length=255)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
