5a3d84f550c7479cfd4556bf88e80d47f0953ad7
Gabriel Akinshola <gabrielakinshola3@gmail.com>
2025-01-31 18:24:12 +0100

    added user profile model init

--- users/models.py
+++ users/models.py
@@ -20,3 +20,21 @@ class CustomUser(AbstractUser):
             raise ValueError("A user cannot be both an instructor and a student.")
         super().save(*args, **kwargs)
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class UserProfile(models.Model):
+    from courses.models import Course
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='userprofile')
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    bio = models.TextField(blank=True, null=True)
+    interests = models.ManyToManyField('Tag', blank=True)
+    completed_courses = models.ManyToManyField(Course, blank=True)
+    last_seen = models.DateTimeField(auto_now=True)     # For "last seen" status
+    status_message = models.TextField(blank=True, null=True)
+    status_image = models.ImageField(upload_to='status/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
