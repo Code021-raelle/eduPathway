70923dc8ba64ee23f955be049a4aae506ac9c4a5
Gabriel Akinshola <gabrielakinshola3@gmail.com>
2025-02-08 14:50:11 +0100

    added star to rating

--- courses/models.py
+++ courses/models.py
@@ -71,6 +71,9 @@ class Review(models.Model):
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.user.username} - {self.rating}⭐"
+
 class Notification(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="notifications")
     thread = models.ForeignKey('DiscussionThread', on_delete=models.Case, null=True, blank=True)
