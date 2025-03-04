917e462fd21724a090c2ce2453f6fc8328b676df
Code021-raelle <gabrielakinshola3@gmail.com>
2025-03-04 15:33:09 +0100

    added the url path to edit profile

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profiles/<str:username>/', views.user_profile, name='user_profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
 ]
