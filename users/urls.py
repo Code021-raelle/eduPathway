bf81537326a43016282f7640eb062a7731c829e8
Code021-raelle <gabrielakinshola3@gmail.com>
2025-03-08 13:45:34 +0100

    added the withdraw cash url

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('profiles/<str:username>/', views.user_profile, name='user_profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('withdraw_cash/', views.withdraw_cash, name='withdraw_cash'),
 ]
