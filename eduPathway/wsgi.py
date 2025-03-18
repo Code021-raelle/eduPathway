4f2ce6e35b2d63551054a353f22c8c334135e3e4
Code021-raelle <gabrielakinshola3@gmail.com>
2025-03-18 18:15:38 +0100

    esgi integrated

--- eduPathway/wsgi.py
+++ eduPathway/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
 
 from django.core.wsgi import get_wsgi_application
 
